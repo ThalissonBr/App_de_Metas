68023afc2cedfe3874665b69744386f790550f44
Thalisson Breno <Breno.thalisson@gmail.com>
2024-09-11 15:54:54 -0300

    fundamentos003

---
+++ index.js
@@ -0,0 +1,14 @@
+let meta = {
+    value: 'Ler um livro todo mes',
+    checked: true,
+}
+
+let metas = [
+    meta, 
+    {
+        value: "Caminhar 20 minutos todos os dias",
+        checked: false
+    }
+]
+console.log(metas[1].value)
+
