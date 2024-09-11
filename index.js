5bef685e262eae978d03236058d6e072aa85188a
Thalisson Breno <Breno.thalisson@gmail.com>
2024-09-11 16:58:52 -0300

    cadastrar meta

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
-function start(){
+const{ select, input } = require('@inquirer/prompts')
+
+let meta = {
+    value: 'Tomar 3L de agua todo dia ',
+    checked: false,
+}
+
+let metas = [ meta ]
+
+const cadastrarMeta = async () => {
+    const meta = await input({message:"Digite a meta: "})
+
+    if(meta.length == 0){
+        console.log("A meta não pode ser vazia.")
+        return
+    }
+
+    metas.push({value:meta, checked:false})
+}
+
+const start = async() => {
+
     while(true){
-        let opcao = "sair"
+
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value:"listar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
+        
         switch(opcao){
             case"cadastrar":
-                console.log("vamos cadastrar")
+                await cadastrarMeta()
+                console.log(metas)
                 break
-            case"lista":
+            case"listar":
                 console.log("vamos listar")
+                break
             case"sair":
+            console.log("Ate a proxima!")
                 return
         }
     }
